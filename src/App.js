1b02d64c044c84f2e42c89728e0d119513331247
Bulent Eroglu <eroglu.bulent98@gmail.com>
2020-04-12 19:11:01 +0100

    basic data displaying, with chart

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({
+      data: fetchedData,
+    });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    console.log(fetchedData);
+
+    console.log(country);
+    // fetch the data
+    // set the data
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart />
+      </div>
+    );
+  }
+}
+
+export default App;
